9af8aa2a3ed067426ba5e3a9271c7b52cb936671
Andrew Naumann <apnaumann@gmail.com>
2015-04-14 14:32:47 -0400

    Add rough implementation of modal popup

--- js/app.js
+++ js/app.js
@@ -1,8 +1,53 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
 $(document).ready(function() {
+  //Init skrollr
   var s = skrollr.init({
     forceHeight: false
   });
+  
+  $(".logo").click(function() {
+    $('html, body').animate({
+      scrollTop: 550
+    }, 500);
+  });
+  
+  
+//  Modal stuff
+  var body = $('body'),
+    main = $('.main'),
+    open_modal = $('.open-modal'),
+    close_modal = $('.close-modal'),
+    modal_container = $('.modal-container'),
+    toggleModal = function() {
+        body.toggleClass('body-locked');
+        modal_container.toggleClass('dp-block');
+    };
+  var pageHeight = body.height();
+  
+  function openModal(modal) {
+    modal.css({top: pageHeight})
+    modal.animate({
+      top: 100
+    }, 500);
+  }
+  
+  open_modal.click(function(event){
+    event.preventDefault();
+    toggleModal();
+    openModal($('.modal'));
+  });
+  
+  close_modal.click(function(event){
+    event.preventDefault();
+    $('.modal').animate({
+      top: pageHeight
+    }, 500, function() {
+      toggleModal();
+    });
+  });
+  
+
 });
